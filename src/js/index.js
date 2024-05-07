a87c458aa878666466d6bfccafe81a8635161315
melevanet <157515892+melevanet@users.noreply.github.com>
2024-05-07 18:18:25 +0000

    melevanet-contadorsimple

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,61 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+function SimpleCounter(props) {
+    return (
+        <div className="bigCounter">
+            <div className="calendar">
+                ⏰
+            </div>
+            <div className="four">{props.digitFour % 10}</div>
+            <div className="three">{props.digitTheer % 10}</div>
+            <div className="two">{props.digitTwo % 10}</div>
+            <div className="one">{props.digitOne % 10}</div>
+
+
+
+        </div>
+    );
+}
+
+SimpleCounter.propTypes = {
+    // You can declare that a prop is a specific JS primitive. By default, these
+    // are all optional.
+    digitFour: PropTypes.number,
+    digitTheer: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
+};
+
+let counter = 0;
+setInterval(function () {
+
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+    console.log(four, three, two, one);
+    counter++;
+
+    ReactDOM.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitTheer={three} digitFour={four} />,
+        document.querySelector("#app"));
+    
+}, 1000);
+
+
+
 //import your own components
-import Home from "./component/home.jsx";
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+
+
+
+
